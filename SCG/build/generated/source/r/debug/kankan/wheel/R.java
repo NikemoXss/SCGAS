e019c9791f5ff5742ad71bbb050179951213ee32
NikemoXss <2461178131@qq.com>
2017-07-13 17:39:40 +0800

    无聊的一天

--- SCG/build/generated/source/r/debug/kankan/wheel/R.java
+++ SCG/build/generated/source/r/debug/kankan/wheel/R.java
@@ -7,8 +7,4 @@
 package kankan.wheel;
 
 public final class R {
-    public static final class drawable {
-        public static final int wheel_bg = 0x7f0202c3;
-        public static final int wheel_val = 0x7f0202c5;
-    }
 }
